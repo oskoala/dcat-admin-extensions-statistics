58b2946cb1e7b45ee09cec432bf987129e8c6942
武志祥 <657873584@qq.com>
2021-09-27 16:01:09 +0800

    Update time-range.blade.php

--- resources/views/time-range.blade.php
+++ resources/views/time-range.blade.php
@@ -6,26 +6,26 @@
             {{\OsKoala\Statistics\StatisticsServiceProvider::setting("title") ?? "考拉开源网站数据统计"}}
         </h4>
         <select id="time-range" class="form-control" style="width: 200px!important;float: right;margin-right: 30px">
-            <option value="{{admin_url('auth/statistics/index?day=1')}}"
+            <option value="{{admin_url('auth/statistics?day=1')}}"
                     @if(isset($_GET['day']) && $_GET['day'] == 1) selected @endif>
                 最近24小时
             </option>
-            <option value="{{admin_url('auth/statistics/index?day=3')}}"
+            <option value="{{admin_url('auth/statistics?day=3')}}"
                     @if(isset($_GET['day']) && $_GET['day'] == 3) selected @endif
             >
                 最近三天
             </option>
-            <option value="{{admin_url('auth/statistics/index?day=7')}}"
+            <option value="{{admin_url('auth/statistics?day=7')}}"
                     @if(isset($_GET['day']) && $_GET['day'] == 7) selected @endif
             >
                 最近7天
             </option>
-            <option value="{{admin_url('auth/statistics/index?day=30')}}"
+            <option value="{{admin_url('auth/statistics?day=30')}}"
                     @if(isset($_GET['day']) && $_GET['day'] == 30) selected @endif
             >
                 最近30天
             </option>
-            <option value="{{admin_url('auth/statistics/index?day=90')}}"
+            <option value="{{admin_url('auth/statistics?day=90')}}"
                     @if(isset($_GET['day']) && $_GET['day'] == 90) selected @endif
             >
                 最近90天
